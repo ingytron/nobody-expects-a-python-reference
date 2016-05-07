eb76fa195a7ce946084ec108face561d877c5cf1
voodoobettie <ingrid@frankenmedia.ca>
2016-05-07 11:49:34 -0700

    Added hangman game

---
+++ hangman-textgame-py3.py
@@ -0,0 +1,129 @@
+Python 3.4.3 (default, Oct 14 2015, 20:28:29) 
+[GCC 4.8.4] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Welcome to the COSC110 Guessing Game!
+
+
+Reading word list...
+Selecting secret word...
+A secret word has been selected...
+
+
+The current hint is ________
+Guesses remaining: 10
+Please enter a letter to guess: 
+>>> ================================ RESTART ================================
+>>> 
+Welcome to the COSC110 Guessing Game!
+
+
+Reading word list...
+Selecting secret word...
+A secret word has been selected...
+
+
+The current hint is _ _ _ _ _ _ _ _ _ 
+Guesses remaining: 10
+Please enter a letter to guess: a
+a does not occur in the secret word
+
+The current hint is _ _ _ _ _ _ _ _ _ 
+Guesses remaining: 9
+Please enter a letter to guess: e
+That is a good guess! e occurs 2 time(s) in the secret word
+
+The current hint is _ e_ _ _ e_ _ _ 
+Guesses remaining: 8
+Please enter a letter to guess: i
+That is a good guess! i occurs 1 time(s) in the secret word
+
+The current hint is _ e_ _ _ ei_ _ 
+Guesses remaining: 7
+Please enter a letter to guess: r
+That is a good guess! r occurs 1 time(s) in the secret word
+
+The current hint is re_ _ _ ei_ _ 
+Guesses remaining: 6
+Please enter a letter to guess: t
+t does not occur in the secret word
+
+The current hint is re_ _ _ ei_ _ 
+Guesses remaining: 5
+Please enter a letter to guess: p
+p does not occur in the secret word
+
+The current hint is re_ _ _ ei_ _ 
+Guesses remaining: 4
+Please enter a letter to guess: s
+That is a good guess! s occurs 1 time(s) in the secret word
+
+The current hint is res_ _ ei_ _ 
+Guesses remaining: 3
+Please enter a letter to guess: c
+c does not occur in the secret word
+
+The current hint is res_ _ ei_ _ 
+Guesses remaining: 2
+Please enter a letter to guess: o
+That is a good guess! o occurs 1 time(s) in the secret word
+
+The current hint is res_ oei_ _ 
+Guesses remaining: 1
+Please enter a letter to guess: h
+That is a good guess! h occurs 1 time(s) in the secret word
+
+The current hint is res_ oei_ _ 
+What is your final guess for the secret word? 
+Sorry, the secret word was: reshoeing
+>>> runGame()
+Welcome to the COSC110 Guessing Game!
+
+
+Reading word list...
+Selecting secret word...
+A secret word has been selected...
+
+
+The current hint is _ _ _ _ _ 
+Guesses remaining: 10
+Please enter a letter to guess: s
+That is a good guess! s occurs 1 time(s) in the secret word
+
+The current hint is s_ _ _ _ 
+Guesses remaining: 9
+Please enter a letter to guess: t
+t does not occur in the secret word
+
+The current hint is s_ _ _ _ 
+Guesses remaining: 8
+Please enter a letter to guess: r
+r does not occur in the secret word
+
+The current hint is s_ _ _ _ 
+Guesses remaining: 7
+Please enter a letter to guess: o
+That is a good guess! o occurs 2 time(s) in the secret word
+
+The current hint is s_ oo_ 
+Guesses remaining: 6
+Please enter a letter to guess: l
+That is a good guess! l occurs 1 time(s) in the secret word
+
+The current hint is s_ ool
+Guesses remaining: 5
+Please enter a letter to guess: t
+That letter has already been guessed!
+
+The current hint is s_ ool
+Guesses remaining: 4
+Please enter a letter to guess: p
+p does not occur in the secret word
+
+The current hint is s_ ool
+Guesses remaining: 3
+Please enter a letter to guess: n
+That is a good guess! n occurs 1 time(s) in the secret word
+Congratulations! You guessed the secret word: snool
+>>> 
